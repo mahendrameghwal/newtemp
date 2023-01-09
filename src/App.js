b3b3b97322a607bcf814a065b6e253ebd8987e37
mahendra meghwal <solankirakesh895@gmail.com>
2023-01-10 02:28:21 +0530

    added some old files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Template1 from "./Template/Template1";
 import Template2 from "./Template/Template2"; 
 import Template3 from "./Template/Template3";
 import Template4 from "./Template/Template4";
+import Template5 from "./Template/Template 5";
+import Template6 from "./Template/Template 6";
+
 
 
 import "./App.css"
@@ -12,6 +15,9 @@ function App() {
      <Template2/>
      <Template3/>
      <Template4/>
+     <Template5/>
+     <Template6/>
+
     </div>
   );
 }
